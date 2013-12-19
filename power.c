8a5bd2166024d8e273fa36110d6b9eedd16ca62f
Mahdi-Rom <mahdi.rom@gmail.com>
2013-12-19 16:17:26 +0000

    remove logspam when mpdecision is not being used

--- power.c
+++ power.c
@@ -161,9 +161,9 @@ static void touch_boost()
 
     snprintf(data, MAX_LENGTH, "1:%d", client);
     rc = sendto(client_sockfd, data, strlen(data), 0, (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
+    //if (rc < 0) {
+        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+    //}
 }
 #endif
 
